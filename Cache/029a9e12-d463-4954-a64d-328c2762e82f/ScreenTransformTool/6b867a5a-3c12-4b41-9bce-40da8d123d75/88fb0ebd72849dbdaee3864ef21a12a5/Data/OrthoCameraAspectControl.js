6fbcbbf381b8f5e93b9a9405c40a0d3991063d9b
Shengyi Z <shengyi.zhong@mail.mcgill.ca>
2026-03-21 13:37:49 -0400

    restart project

--- Cache/029a9e12-d463-4954-a64d-328c2762e82f/ScreenTransformTool/6b867a5a-3c12-4b41-9bce-40da8d123d75/88fb0ebd72849dbdaee3864ef21a12a5/Data/OrthoCameraAspectControl.js
+++
@@ -1,6 +0,0 @@
-//@input Component.Camera camera
-//@input Asset.Texture overlayTarget
-
-script.createEvent("UpdateEvent").bind(function() {
-    script.camera.aspect = script.overlayTarget.getWidth() / script.overlayTarget.getHeight();
-})
